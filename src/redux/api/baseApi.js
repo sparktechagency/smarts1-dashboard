8498e8db395b8623cd40d42da50c271c9af55eed
mdbayzid2025 <ssmdbayzid23@gmail.com>
2025-09-24 17:27:47 +0600

    update

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -24,6 +24,7 @@ export const api = createApi({
 // export const imageUrl = "http://206.189.231.81:5000";
 // export const imageUrl = "http://10.10.7.79:7003";
 export const imageUrl = "http://157.241.34.32:3000";
-export const socketUrl = "ws://10.10.7.79:6003";
+// export const socketUrl = "ws://10.10.7.79:6003";
+export const socketUrl = import.meta.env.BASE_URL;
 
 // http://10.10.7.79:7003/api/v1
