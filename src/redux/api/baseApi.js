adac1abc30e97161269ab249f7ff7ff418944a41
asifaowadud <asifaowadud@gmail.com>
2025-09-25 16:56:55 +0600

    mend

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -6,8 +6,8 @@ export const api = createApi({
     baseQuery: fetchBaseQuery({
         // baseUrl: "http://206.189.231.81:5000/api/v1"
         // baseUrl: "http://10.10.7.79:7003/api/v1",        
-        baseUrl: import.meta.env.BASE_URL,        
-        // baseUrl: 'http://157.241.91.8:3000/api/v1',        
+        // baseUrl: import.meta.env.BASE_URL,        
+        baseUrl: 'http://157.241.91.8:3000/api/v1',        
          prepareHeaders: (headers) => {
       headers.set("ngrok-skip-browser-warning", "true");
       const token = Cookies.get("accessToken");
@@ -22,7 +22,7 @@ export const api = createApi({
     endpoints: () => ({})
 });
 
-export const imageUrl = import.meta.env.BASE_URL;
-export const socketUrl = import.meta.env.SOCKET_URL;
-// export const imageUrl = 'http://157.241.91.8:3000';
-// export const socketUrl = 'http://157.241.91.8:3000/api/v1';
+// export const imageUrl = import.meta.env.BASE_URL;
+// export const socketUrl = import.meta.env.SOCKET_URL;
+export const imageUrl = 'http://157.241.91.8:3000';
+export const socketUrl = 'http://157.241.91.8:3000/api/v1';
