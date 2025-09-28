85d89bc1ece6c4b4dd1889e3cab3cf6d6d629e5c
Asif A Owadud <72104020+abuabddullah@users.noreply.github.com>
2025-09-28 10:20:51 +0600

    Update vite.config.js
    preview added

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,9 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 3001,
   },
+  preview: {
+    host: "0.0.0.0",
+    port: 3001,
+    strictPort: true, // optional: fail instead of falling back if 3001 is taken
+  },
 });
